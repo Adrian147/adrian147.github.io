3fcad087f8b4ccc15a2b9884cd5ce62450fcbd30
Adrian McDonald <adrian47@mcdonald@gmail.com>
2016-12-15 00:09:08 +0530

    Basic Responsive Mockup
    Just the basic text without mobile support, fancy animations or pictures.
    Contentwise, no timeline and previous work.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.SlideHeight1').css("height", screen.availHeight);
+    $('.SlideHeight2').css("height", 2*screen.availHeight);
+    $('.SlideHeight3').css("height", 3*screen.availHeight);
+});
